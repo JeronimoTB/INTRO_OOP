fa3d432648d3d5eb2eacbe65913afb8c62761dc1
JeronimoTB <jerotobe14@gmail.com>
2023-03-15 11:20:33 -0500

    Loq sea

---
+++ OperacionesAritmeticas.cs
@@ -0,0 +1,48 @@
+class OperacionesAritmeticas
+{
+    //PROPIEDADES (Atributos)
+    //mod acc  tipo dato  nombrePropieda (Atributo)
+    //Metodo recibir (set) Metodo entregar (get)
+    //Valor de la Propiedad
+    public int numero_uno { get;set; }
+    public int numero_dos { get;set; }
+
+    //METODOS (Funciones o Acciones)
+    //Metodo sin parametros
+    //mod acc tipo retorno nombreMetodo
+    public void sumar_num(){
+        //Operaciones
+        int resul = numero_uno + numero_dos;
+
+        //Salida
+        Console.WriteLine("El resultado de la suma es " + resul);
+    }
+
+    public void restar_num(){
+        //Operaciones
+        int resul = numero_uno - numero_dos;
+
+        //Salida
+        Console.WriteLine("El resultado de la resta es " + resul);
+    }
+
+    public void multiplicar_num(){
+    int resul = numero_uno * numero_dos;
+
+    Console.WriteLine("El resultado de la multiplicacion es " + resul);
+    }
+
+    public void dividir_num(){
+    int resul = numero_uno / numero_dos;
+
+    Console.WriteLine("El resultado de la division es " + resul);
+    }
+
+    //Metodo Constructor 
+    /*OperacionesAritmeticas(){
+        gsgsgs
+        gsgsgsg
+        fasfdafsad
+    }
+    */
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Numero Uno");
+int num_uno = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Numero Dos");
+int num_dos = int.Parse(Console.ReadLine());
+
+//Instanciar, crear un objeto o hacer una copia de la clase
+//Forma orientada a objetos
+//Instanciar la  class OperacionesAritmeticas (objeto de la class)
+//Class  objeto(instancia) (= new)sonPalabrasReservadas metodoConstructorDeLaClass
+
+var operacion_uno = new OperacionesAritmeticas(){
+    //oper = operacion,
+    numero_uno = num_uno,
+    numero_dos = num_dos
+};
+
+var operacion_dos = new OperacionesAritmeticas(){
+    //oper = operacion,
+    numero_uno = num_uno,
+    numero_dos = num_dos
+};
+
+
+//Ver los resultados de la suma
+operacion_uno.sumar_num();
+operacion_uno.restar_num();
+operacion_uno.multiplicar_num();
+operacion_uno.dividir_num();
