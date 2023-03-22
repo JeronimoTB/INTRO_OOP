3cc33ef63dd61acf73be38d5426ceaa608626b1f
JeronimoTB <jerotobe14@gmail.com>
2023-03-22 11:06:21 -0500

    Ejercicio Hotel WC en C#

--- OperacionesAritmeticas.cs
+++ OperacionesAritmeticas.cs
@@ -6,38 +6,129 @@ class OperacionesAritmeticas
     //Valor de la Propiedad
     public int numero_uno { get;set; }
     public int numero_dos { get;set; }
+    public int operation { get; set; }
 
     //METODOS (Funciones o Acciones)
     //Metodo sin parametros
     //mod acc tipo retorno nombreMetodo
-    public void sumar_num(){
+   
+    //METODO QUE REALIZA SUMA
+    public int sumar_num(int n1, int n2){
         //Operaciones
-        float resul = numero_uno + numero_dos;
+        int resultadoSuma = 0;
+        resultadoSuma = n1 + n2;
+
+        //Retornar Resultado (Se realiza mas no muestra nada)
+        return resultadoSuma;
+    }
+    
+    //METODO QUE IMPRIME LA SUMA
+    public void ImprimirSuma(){
+        //Operaciones
+        int resulS = sumar_num(numero_uno, numero_dos);
 
         //Salida
-        Console.WriteLine("El resultado de la suma es " + resul);
+        Console.WriteLine($"El resultado de la suma es: {resulS}");
     }
 
-    public void restar_num(){
+    //METODO QUE REALIZA RESTA
+    public int restar_num(int n1, int n2){
         //Operaciones
-        float resul = numero_uno - numero_dos;
+        int resultadoResta = 0;
+        resultadoResta = n1 - n2;
+
+        //Retornar Resultado (Se realiza mas no muestra nada)
+        return resultadoResta;
+    }
+
+    //METODO QUE IMPRIME LA RESTA
+    public void ImprimirResta(){
+        //Operaciones
+        int resulR = restar_num(numero_uno, numero_dos);
 
         //Salida
-        Console.WriteLine("El resultado de la resta es " + resul);
+        Console.WriteLine($"El resultado de la Resta es: {resulR}");
     }
 
-    public void multiplicar_num(){
-    float resul = numero_uno * numero_dos;
 
-    Console.WriteLine("El resultado de la multiplicacion es " + resul);
+    //METODO QUE REALIZA MULTIPLICACION
+    public int multiplicar_num(int n1, int n2){
+        //Operaciones
+        int resultadoMulti = 0;
+        resultadoMulti = n1 * n2;
+
+        //Retornar Resultado (Se realiza mas no muestra nada)
+        return resultadoMulti;
     }
 
-    public void dividir_num(){
-    float resul = numero_uno / numero_dos;
+    //METODO QUE IMPRIME LA MULTIPLICACION
+    public void ImprimirMultiplicacion(){
+        //Operaciones
+        int resulM = multiplicar_num(numero_uno, numero_dos);
 
-    Console.WriteLine("El resultado de la division es " + resul);
+        //Salida
+        Console.WriteLine($"El resultado de la Multiplicacion es: {resulM}");
     }
 
+    //METODO QUE REALIZA DIVISION
+    public int dividir_num(int n1, int n2){
+        //Operaciones
+        int resultadoDivi = 0;
+        resultadoDivi = n1 / n2;
+
+        //Retornar Resultado (Se realiza mas no muestra nada)
+        return resultadoDivi;
+    }
+
+    public void ImprimirDivision(){
+        //Operaciones
+        int resulD = dividir_num(numero_uno, numero_dos);
+
+        //Salida
+        Console.WriteLine($"El resultado de la Multiplicacion es: {resulD}");
+    }
+
+      public void operacionRealizar(){
+        switch (operation)
+        {
+            case 1:
+                ImprimirSuma();
+                break;
+            case 2:
+                ImprimirResta();
+                break;
+            case 3:
+                ImprimirMultiplicacion();
+                break;
+            case 4:
+                ImprimirDivision();
+                break;
+            default:
+                Console.WriteLine("Opción Inválida");
+                break;
+        }
+    }
+    
+     /*
+    public void operacionRealizar (){
+        if (operation == 1){
+            sumar_num();
+        }
+        else if (operation == 2){
+            restar_num();
+        }
+        else if (operation == 3){
+            multiplicar_num();
+        }
+        else if (operation == 4){
+            dividir_num();
+        }
+        else{
+            Console.WriteLine("Operacion Invalida");
+        }
+    }
+    */
+    
     //Metodo Constructor 
     /*OperacionesAritmeticas(){
         gsgsgs
--- Program.cs
+++ Program.cs
@@ -1,30 +1,47 @@
-﻿Console.WriteLine("Hola Mundo");
-Console.WriteLine("Numero Uno");
-int num_uno = int.Parse(Console.ReadLine());
+﻿// Console.WriteLine("Hola, ingresa 2 números...");
+// Console.WriteLine("Numero Uno");
+// int num_uno = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Numero Dos");
-int num_dos = int.Parse(Console.ReadLine());
+// Console.WriteLine("Numero Dos");
+// int num_dos = int.Parse(Console.ReadLine());
 
-//Instanciar, crear un objeto o hacer una copia de la clase
-//Forma orientada a objetos
-//Instanciar la  class OperacionesAritmeticas (objeto de la class)
-//Class  objeto(instancia) (= new)sonPalabrasReservadas metodoConstructorDeLaClass
+// Console.WriteLine("Que operacion desea realizar?\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division");
+// int opera = int.Parse(Console.ReadLine());
 
-var operacion_uno = new OperacionesAritmeticas(){
-    //oper = operacion,
-    numero_uno = num_uno,
-    numero_dos = num_dos
+// //Instanciar, crear un objeto o hacer una copia de la clase
+// //Forma orientada a objetos
+// //Instanciar la  class OperacionesAritmeticas (objeto de la class)
+// //Class  objeto(instancia) (= new)sonPalabrasReservadas metodoConstructorDeLaClass
+
+// var operacion_uno = new OperacionesAritmeticas(){
+//     //oper = operacion,
+//     numero_uno = num_uno,
+//     numero_dos = num_dos,
+//     operation = opera
+// };
+
+
+// //Ejemplo de que se pueden sacar muchas instancias, más este no sirve/se utiliza en este ejemplo
+// var operacion_dos = new OperacionesAritmeticas(){
+//     //oper = operacion,
+//     numero_uno = num_uno,
+//     numero_dos = num_dos
+// };
+
+// //Para mostrar los resultados de la suma, se debe hacer la operacion a traves
+// //Del Objeto ya que es la que ahora tiene los atributos
+
+// operacion_uno.operacionRealizar();
+Console.WriteLine("Buen día, Bienvenido a el Hotel WC");
+Console.WriteLine("Cuantas Días desea Hospedarse?");
+float num_dia = float.Parse(Console.ReadLine());
+
+Console.WriteLine("A qué tarifa pertenecen para Hospedarse?\n1. Individual\n2. Doble\n3. Familiar");
+float caso_per = float.Parse(Console.ReadLine());
+
+var operacion_uno = new OperacionesHotel(){
+    caso_personas = caso_per,
+    numero_dias = num_dia
 };
 
-var operacion_dos = new OperacionesAritmeticas(){
-    //oper = operacion,
-    numero_uno = num_uno,
-    numero_dos = num_dos
-};
-
-
-//Ver los resultados de la suma
-operacion_uno.sumar_num();
-operacion_uno.restar_num();
-operacion_uno.multiplicar_num();
-operacion_uno.dividir_num();
+operacion_uno.OperacionTarifas();
